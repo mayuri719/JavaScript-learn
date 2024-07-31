96ca8349f4b849c1091680f13f61673ded208224
mayuri719 <mayurihunge26@gmail.com>
2024-07-31 07:52:49 +0530

    Javascript variables
    Javascript variables

---
+++ 01-Variable.js
@@ -0,0 +1,29 @@
+var fname;
+console.log(fname);//it will return undefined because we not innitialize any value.
+fname = "mayu";
+console.log(fname);
+var lname = "hunge";
+console.log(fname + " " + lname);
+fname = "rohan";    //updating the value
+console.log(fname + " " + lname);
+console.log("Pandharpur");
+console.log("Shinshan");
+console.log("India");
+console.log("VVP");
+console.log("SVERI");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
