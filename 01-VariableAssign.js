9392f33f3d9dcfe5ddaa31a68428180afd933509
mayuri719 <mayurihunge26@gmail.com>
2024-07-31 16:32:11 +0530

    variable and datatype assignment
    variable and datatype assignment

---
+++ 01-VariableAssign.js
@@ -0,0 +1,39 @@
+console.log("==========================Q.1===========================");
+var myFullName;
+console.log("Value before initialization:",myFullName);
+
+myFullName="Mayuri Hunge";
+console.log("Value After initialization :",myFullName);
+
+
+myFullName="Mayuri Dattatray Hunge";
+console.log("Updated value is :",myFullName);
+
+console.log("==========================Q.2===========================");
+
+ var myLove="I love only JS";
+ console.log("variable with initial value :",myLove);
+
+ console.log("==========================Q.3===========================");
+
+ var firstName="Mayuri";
+ console.log("FirstName is :",firstName);
+
+
+ var lastName="Hunge";
+ console.log("LastName is :",lastName);
+
+ var age=23;
+console.log("Age is",age);
+
+var address ="Pune";
+console.log("Address is :",address);
+
+var pincode= "411014";
+console.log("Pincode is :",pincode);
+
+var gender="Female";
+console.log("Gender is :",gender);
+
+ var IsMarried= "N";
+ console.log("Mariied Y/N :",IsMarried);
---
+++ typeOf.js
@@ -0,0 +1,26 @@
+var bankName ;
+console.log(bankName , typeof bankName);
+
+console.log("==================================================================");
+
+bankName="SBI Bank";
+console.log("Value afer iniialization :",bankName ," || Datatype is :",typeof bankName);
+console.log("==================================================================");
+var marks="90%";
+console.log("Marks are :", marks ,"|| Datatype is :", typeof marks);
+
+console.log("==================================================================");
+
+var isWorking;
+console.log("value before initializarion :",isWorking,"  ||  Datatype is :",typeof isWorking);
+
+console.log("==================================================================");
+
+var totalCount="Hundred and seven";
+console.log("Value is :",totalCount, "  ||  Datatype is :", typeof totalCount);
+
+console.log("==================================================================");
+
+totalCount= 107;
+console.log("updated Value is : ",totalCount,"  ||  Datatype is :",typeof totalCount);
+
