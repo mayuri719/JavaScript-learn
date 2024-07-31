41ed4bd3e260d314775be2c38b44296e521d2abe
mayuri719 <mayurihunge26@gmail.com>
2024-07-31 08:45:42 +0530

    Create 01-dataType.js
    new file

---
+++ 01-dataType.js
@@ -0,0 +1,31 @@
+//Primitive Datatype : Number, String and Boolean
+console.log("================Number datatype=================");
+
+var age=22;
+console.log("Age is",age);
+console.log("Datatype of Age is : ",typeof age);
+
+console.log("================String datatype=================");
+var city="pune";
+console.log("City Name is :",city);
+console.log("DataType of City is :",typeof city);
+
+var marks="89";
+console.log("Marks are :", marks);
+console.log("Datatype Of Marks is :", typeof marks);
+
+console.log("================Boolean datatype=================");
+var IsVisible=true;
+console.log(IsVisible);
+console.log("Datatype of IsVisible is :", typeof IsVisible);
+
+//=================Special Datatypes : undefined , null==============
+
+console.log("================Special datatype=================");
+var country;//it means we declare variable but not assign any value.
+console.log("Country is :",country);
+console.log("Datatype of Country is :", typeof country);
+
+var village = null;
+console.log("Country is :",village);
+console.log("Datatype of Village is :", typeof village);
